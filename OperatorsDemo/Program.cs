e47acd469b0685bee34c198ed8cd40beb6d9fd21
Aaron Tanguma <the_guma@yahoo.com>
2025-12-11 18:54:51 -0700

    added notes on operators in C#

---
+++ OperatorsDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OperatorsDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Operators
+        
+        // Assigment operator = 
+        
+        // int x = 5;
+        
+        // Arithmetic operators
+
+        int addition = 5 + 5;
+        int subtraction = 5 - 5;
+        int multiplication = 5 * 5;
+        int division = 5 / 5;
+        int modulus = 6 % 5; // gives the remainder after division, output = 1, good for checking if a number is even or odd
+        bool isEven = 4 % 2 == 0;
+    }
+}
