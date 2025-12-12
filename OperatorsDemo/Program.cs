b4d59a1381e6069d6cf5a737b1640be2433113f1
Aaron Tanguma <the_guma@yahoo.com>
2025-12-11 19:03:20 -0700

    Input notes on unary operator example

--- OperatorsDemo/Program.cs
+++ OperatorsDemo/Program.cs
@@ -17,6 +17,21 @@ class Program
         int multiplication = 5 * 5;
         int division = 5 / 5;
         int modulus = 6 % 5; // gives the remainder after division, output = 1, good for checking if a number is even or odd
-        bool isEven = 4 % 2 == 0;
+        // bool isEven = 4 % 2 == 0; // example of using a modulus to check is even
+        
+        // Unary operators
+        int x = 3;
+        int y = ++x; // example of pre increment
+
+        Console.WriteLine(x); 
+        Console.WriteLine(y);
+
+        int a = 3;
+        int b = a++; // example of post increment
+
+        Console.WriteLine(a); 
+        Console.WriteLine(b);
+        
+        
     }
 }
